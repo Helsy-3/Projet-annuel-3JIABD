0cdf072de2d3ade33732b226955e6db1fc1b78f8
martinoauriane <189979398+martinoauriane@users.noreply.github.com>
2026-09-23 12:59:40 +0200

    rewriting entrainer_perceptron function

--- C-algorithms/ETAPE1.basicPerceptron.c
+++ C-algorithms/ETAPE1.basicPerceptron.c
@@ -1,45 +1,55 @@
 #include <stdio.h>
+#include <string.h>
 
-// Cas de test officiel du prof ("Linear Simple")
-int X_data[3][2] = {{1, 1}, {2, 3}, {3, 3}};
-int Y_pred[3] = {1, -1, -1};
+// EXEMPLE
+//int X_data[3][2] = {{1, 1}, {2, 3}, {3, 3}};
+//int Y_pred[3] = {1, -1, -1};
 
-// Entraine un perceptron simple sur X_data et renvoie le nombre d'erreurs finales
-int run_perceptron(void)
+int entrainer_perceptron(double learning_rate, double *bias, int rep, int X_data_lignes, int X_data_colonnes, double X_data[X_data_lignes][X_data_colonnes], double Y_pred[])
 {
-    double weights[2] = {0.0, 0.0};
-    double bias = 0.0;
-    double learning_rate = 0.1;
+    size_t dataset_size = X_data_lignes; // taille du dataset
+    size_t poids_nb = X_data_colonnes;
 
-    for (int rep = 0; rep < 1000; rep++)
+    // Dans le data set, chaque donnée est un vecteur avec des caractéristiques [1, 2, 3, 4]
+    // A chaque vecteur correspond un poids. Il y a autant de poids qu'il y a de caractéristiques par donnée du dataset.
+
+    // Il faut qu'il y ait autant de poids qu'il y a de caractéristiques pour une donnée.
+    // Exemple 1: une maison avec deux caractéristiques: la superficie et le nombre d'étages : 2 caractéristiques => donc il faut 2 poids.
+    // Exemple 2: une photo de bouteille avec 32 pixels : 32 caractéristiques => donc il faut 32 poids.
+
+    double poids[poids_nb];
+    memset(poids, 0, sizeof(poids)); // on initialise le tableau de poids à 0.
+    int total_errors = 0;
+
+    for (int i = 0; i < rep; i++) // on entraine le modèle autant de fois qu'il y a de répétitions
     {
-        int total_errors = 0;
-        for (int i = 0; i < 3; i++)
+        int total_errors = 0; // on ca comptabiliser le nombre d'erreurs à chaque fois en comparant Y_pred (le résultat attendu)
+                              // avec le résultat obtenu avec les poids initialisés à 0.
+
+        for (int i = 0; i < dataset_size; i++) // on veut itérer sur toutes les données du data_set
         {
-            double linear_output = X_data[i][0] * weights[0] + X_data[i][1] * weights[1] + bias;
-            int pred = (linear_output >= 0) ? 1 : -1;
+            double linear_output = 0; // un seul neurone
 
-            if (pred != Y_pred[i])
+            for (int y = 0; y < poids_nb; y++)
             {
-                weights[0] += learning_rate * Y_pred[i] * X_data[i][0];
-                weights[1] += learning_rate * Y_pred[i] * X_data[i][1];
-                bias += learning_rate * Y_pred[i];
-                total_errors++;
+                linear_output += X_data[i][y] * poids[y]; // un seul neurone donc on aditionne chaque membre du vecteur et ça donne la sortie de l'unique neurone
             }
-        }
-        if (total_errors == 0)
-            break;
-    }
 
-    int erreurs_finales = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        double sortie = X_data[i][0] * weights[0] + X_data[i][1] * weights[1] + bias;
-        int test_pred = (sortie >= 0) ? 1 : -1;
-        if (test_pred != Y_pred[i])
-            erreurs_finales++;
+            linear_output = linear_output + *bias; // sortie de l'unique neurone
+            
+            int pred = (linear_output >= 0) ? 1 : -1; // prediction du modèle
+            if (pred != Y_pred[i])
+                { // si la prédiction obtenue est différente de la prédiction attendue
+                    for(int y = 0; y < poids_nb ; y++){
+                    poids[y] += learning_rate * Y_pred[i] * X_data[i][y];
+                    }
+                    *bias += learning_rate * Y_pred[i];
+                    total_errors++;
+                }
+            
+        }
     }
-    return erreurs_finales;
+    return total_errors;
 }
 
 // Entraine un perceptron sur des donnees envoyees depuis Python (vraies photos du dataset)
